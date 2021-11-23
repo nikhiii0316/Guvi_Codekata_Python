e7cbdf518ffcf0c623a6c1b9b25f85e6efb576b2
Nikhilesh Kannan M <nikhidevilliers0301@gmail.com>
2021-11-23 21:17:35 +0530

    Update get input and print.py

--- Input_output/get input and print.py
+++ Input_output/get input and print.py
@@ -1,16 +1,15 @@
-***Write a code to get the input in the given format and print the output in the given format
+# Write a code to get the input in the given format and print the output in the given format
+# Input Description:
+# To take an integer value
 
-Input Description:
-To take an integer value
+# Output Description:
+# Print the integer value
 
-Output Description:
-Print the integer value
+# Sample Input :
+  2
+# Sample Output :
+  2
 
-Sample Input :
-2
-Sample Output :
-2
-***
-CODE :
+# CODE :
 a=int(input())
 print(a)
