6e045f05620b640c6839dcaafa8003c39e3377ab
Nikhilesh Kannan M <nikhidevilliers0301@gmail.com>
2021-11-28 22:34:06 +0530

    Create 1.py

---
+++ Absolute Beginner/1.py
@@ -0,0 +1,22 @@
+'''You are given Two Numbers, A and B. If C = A + B. Find C.
+
+Note: Round off the output to a single decimal place.
+
+Input Description:
+You are provided with two numbers A and B.
+
+Output Description:
+Find the sum of the two numbers (A + B)
+
+Sample Input :
+1
+1
+Sample Output :
+2'''
+
+code:
+  
+a = input()
+b = input()
+c = float(a) + float(b)
+print(c)
