2d772f9e963f3c0e3613df9c49c06328252cd7b2
Nikhilesh Kannan M <nikhidevilliers0301@gmail.com>
2021-12-05 14:55:10 +0530

    Create 7.py

---
+++ Absolute Beginner/7.py
@@ -0,0 +1,21 @@
+'''You are provided with the radius of a circle "A". Find the length of its circumference.
+
+Note: In case the output is coming in decimal, roundoff to 2nd decimal place. In case the input is a negative number, print "Error".
+
+Input Description:
+The Radius of a circle is provided as the input of the program.
+
+Output Description:
+Calculate and print the Circumference of the circle corresponding to the input radius up to two decimal places.
+
+Sample Input :
+2
+Sample Output :
+12.57'''
+
+code:
+  
+A = float(input())
+C = 2*(22*A)
+D = C/7
+print(round(D,2))
